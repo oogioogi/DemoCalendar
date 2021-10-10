3828aac7efeb391360add6d40b6e66c387e91d71
oogioogi <detacl73@gmail.com>
2021-10-10 19:47:07 +0900

    Initial Commit

---
+++ DemoCalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoCalendar
+//
+//  Created by 이용석 on 2021/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoCalendar/DemoCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCalendarApp.swift
+//  DemoCalendar
+//
+//  Created by 이용석 on 2021/10/10.
+//
+
+import SwiftUI
+
+@main
+struct DemoCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
